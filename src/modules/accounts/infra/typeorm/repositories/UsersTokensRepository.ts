2e5d353dbab363cdd97639ac0adf7896ea65ca48
IgorSouzaBezerra <igor.souza1.bezerra@gmail.com>
2021-05-14 17:22:14 -0300

    (feat): reset password 🔒

--- src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -39,8 +39,13 @@ class UsersTokensRepository implements IUsersTokensRepository {
     return userTokens;
   }
 
-  async deleteById(id: string): Promise<void> {
+  public async deleteById(id: string): Promise<void> {
     await this.repository.delete(id);
+  }
+
+  public async findByRefreshToken(refresh_token: string): Promise<UserTokens> {
+    const userToken = await this.repository.findOne({ refresh_token });
+    return userToken;
   }
 }
 
--- src/shared/container/provider/DateProvider/implementations/DayJsDateProvider.ts
+++ src/shared/container/provider/DateProvider/implementations/DayJsDateProvider.ts
@@ -35,6 +35,10 @@ class DayJsDateProvider implements IDateProvider {
   public addHours(hours: number): Date {
     return dayjs().add(hours, "hour").toDate();
   }
+
+  public compareIfBefore(start_date: Date, end_date: Date): boolean {
+    return dayjs(start_date).isBefore(end_date);
+  }
 }
 
 export { DayJsDateProvider };
